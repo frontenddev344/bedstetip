0e38f2bf5775e8f52c9dfa25aca6d52d99ee5aa0
Dev avology <devavology12@gmail.com>
2024-09-30 11:35:29 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bedstetip
+
+https://www.canva.com/design/DAGSNfIoKMc/YbCM0Gbb8M75rbWkEZ07wQ/edit
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+// Toggle Menu Functionaliy Start
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".menu-icon").addEventListener("click", function () {
+    document.body.classList.add("menuToggle");
+  });
+
+  document.querySelector(".close-icon").addEventListener("click", function () {
+    document.body.classList.remove("menuToggle");
+  });
+});
+// Toggle Menu Functionaliy End
+
+
+// AOS JS Start
+AOS.init();
+// AOS JS Start
+
+
+//Cookies JS Start
+const cookiesBox = document.querySelector('.wrapper'),
+  buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
+const executeCodes = () => {
+  if (document.cookie.includes('AlexGolovanov')) return;
+  cookiesBox.classList.add('show');
+
+  // ---- ---- Button ---- ---- //
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      cookiesBox.classList.remove('show');
+
+      // ---- ---- Time ---- ---- //
+      if (button.id == 'acceptBtn') {
+        document.cookie =
+          'cookieBy= AlexGolovanov; max-age=' + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+window.addEventListener('load', executeCodes);
+// //Cookies JS End
+
